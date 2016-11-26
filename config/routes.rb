3e8bc2ed16e6ca0d1354d184059149e9da8803c8
caiofilipemr <caiofilipemr@gmail.com>
2016-11-26 19:05:33 -0300

    Correção do bug do SignOut

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'users/sign_in'
   resources :tasks
 
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  root to: 'tasks#index'
 end
