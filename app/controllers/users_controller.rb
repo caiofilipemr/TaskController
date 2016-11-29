3891fbbcaf6b74579392ccb64e63731a3b4d60e5
caiofilipemr <caiofilipemr@gmail.com>
2016-11-29 20:30:31 -0300

    Adicionado tela de gerência de usuários

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,25 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+        format.json { render :index, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_type)
   end
 end
